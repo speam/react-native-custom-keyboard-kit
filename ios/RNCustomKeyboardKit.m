8a3f35d1b06d13b12a393a632064920522012798
Stephen Sun <981152591@qq.com>
2023-02-08 15:46:44 +0800

    fix bugs

--- ios/RNCustomKeyboardKit.m
+++ ios/RNCustomKeyboardKit.m
@@ -11,91 +11,102 @@
 
 - (dispatch_queue_t)methodQueue
 {
-	return dispatch_get_main_queue();
+    return dispatch_get_main_queue();
 }
 
 RCT_EXPORT_MODULE(CustomKeyboardKit)
 
 RCT_EXPORT_METHOD(install:(nonnull NSNumber *)reactTag withType:(nonnull NSString *)keyboardType)
 {
-  UIView* inputView = [[RCTRootView alloc] initWithBridge:_bridge
-                                               moduleName:@"CustomKeyboardKit"
-                                        initialProperties:@{ @"tag": reactTag, @"type": keyboardType }];
-
-  RCTSinglelineTextInputView *view = (RCTSinglelineTextInputView*)[_bridge.uiManager viewForReactTag:reactTag];
-
+    UIView *keyboardView = [[RCTRootView alloc] initWithBridge:_bridge
+                                                    moduleName:@"CustomKeyboardKit"
+                                             initialProperties:@{ @"tag": reactTag, @"type": keyboardType }];
+    keyboardView.autoresizingMask = UIViewAutoresizingNone;
+    keyboardView.frame = CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, 252);
     
-  [view.backedTextInputView setInputAccessoryView:inputView];
-    
-  [view reloadInputViews];
+    RCTSinglelineTextInputView *view = (RCTSinglelineTextInputView*)[_bridge.uiManager viewForReactTag:reactTag];
+    [view.backedTextInputView setInputView:keyboardView];
+    [view reloadInputViews];
 }
 
 RCT_EXPORT_METHOD(uninstall:(nonnull NSNumber *)reactTag)
 {
-  UITextView *view = (UITextView*)[_bridge.uiManager viewForReactTag:reactTag];
-
-  view.inputView = nil;
-  [view reloadInputViews];
+    UITextField *view = (UITextField *)(((RCTBaseTextInputView*)[_bridge.uiManager viewForReactTag:reactTag]).backedTextInputView);
+    view.inputView = nil;
+    [view reloadInputViews];
 }
 
-RCT_EXPORT_METHOD(insertText:(nonnull NSNumber *)reactTag withText:(NSString*)text) {
-  UITextView *view = (UITextView*)[_bridge.uiManager viewForReactTag:reactTag];
-
-  [view replaceRange:view.selectedTextRange withText:text];
+RCT_EXPORT_METHOD(insertText:(nonnull NSNumber *)reactTag withText:(NSString*)text)
+{
+    UITextField *view = (UITextField *)(((RCTBaseTextInputView*)[_bridge.uiManager viewForReactTag:reactTag]).backedTextInputView);
+    [view replaceRange:view.selectedTextRange withText:text];
 }
 
-RCT_EXPORT_METHOD(backSpace:(nonnull NSNumber *)reactTag) {
-  UITextView *view = (UITextView*)[_bridge.uiManager viewForReactTag:reactTag];
-
-  UITextRange* range = view.selectedTextRange;
-  if ([view comparePosition:range.start toPosition:range.end] == 0) {
-    range = [view textRangeFromPosition:[view positionFromPosition:range.start offset:-1] toPosition:range.start];
-  }
-  [view replaceRange:range withText:@""];
+RCT_EXPORT_METHOD(backSpace:(nonnull NSNumber *)reactTag)
+{
+    UITextField *view = (UITextField *)(((RCTBaseTextInputView*)[_bridge.uiManager viewForReactTag:reactTag]).backedTextInputView);
+    
+    UITextRange* range = view.selectedTextRange;
+    if ([view comparePosition:range.start toPosition:range.end] == 0) {
+        range = [view textRangeFromPosition:[view positionFromPosition:range.start offset:-1] toPosition:range.start];
+    }
+    [view replaceRange:range withText:@""];
 }
 
-RCT_EXPORT_METHOD(doDelete:(nonnull NSNumber *)reactTag) {
-  UITextView *view = (UITextView*)[_bridge.uiManager viewForReactTag:reactTag];
-
-  UITextRange* range = view.selectedTextRange;
-  if ([view comparePosition:range.start toPosition:range.end] == 0) {
-    range = [view textRangeFromPosition:range.start toPosition:[view positionFromPosition: range.start offset: 1]];
-  }
-  [view replaceRange:range withText:@""];
+RCT_EXPORT_METHOD(doDelete:(nonnull NSNumber *)reactTag)
+{
+    UITextField *view = (UITextField *)(((RCTBaseTextInputView*)[_bridge.uiManager viewForReactTag:reactTag]).backedTextInputView);
+    
+    UITextRange* range = view.selectedTextRange;
+    if ([view comparePosition:range.start toPosition:range.end] == 0) {
+        range = [view textRangeFromPosition:range.start toPosition:[view positionFromPosition: range.start offset: 1]];
+    }
+    [view replaceRange:range withText:@""];
 }
 
-RCT_EXPORT_METHOD(moveLeft:(nonnull NSNumber *)reactTag) {
-  UITextView *view = (UITextView*)[_bridge.uiManager viewForReactTag:reactTag];
-
-  UITextRange* range = view.selectedTextRange;
-  UITextPosition* position = range.start;
-
-  if ([view comparePosition:range.start toPosition:range.end] == 0) {
-      position = [view positionFromPosition:position offset:-1];
-  }
-
-  view.selectedTextRange = [view textRangeFromPosition: position toPosition:position];
+RCT_EXPORT_METHOD(moveLeft:(nonnull NSNumber *)reactTag)
+{
+    UITextField *view = (UITextField *)(((RCTBaseTextInputView*)[_bridge.uiManager viewForReactTag:reactTag]).backedTextInputView);
+    
+    UITextRange* range = view.selectedTextRange;
+    UITextPosition* position = range.start;
+    
+    if ([view comparePosition:range.start toPosition:range.end] == 0) {
+        position = [view positionFromPosition: position offset: -1];
+    }
+    
+    view.selectedTextRange = [view textRangeFromPosition: position toPosition:position];
 }
 
-RCT_EXPORT_METHOD(moveRight:(nonnull NSNumber *)reactTag) {
-  UITextView *view = (UITextView*)[_bridge.uiManager viewForReactTag:reactTag];
-
-  UITextRange* range = view.selectedTextRange;
-  UITextPosition* position = range.end;
-
-  if ([view comparePosition:range.start toPosition:range.end] == 0) {
-    position = [view positionFromPosition: position offset: 1];
-  }
-
-  view.selectedTextRange = [view textRangeFromPosition: position toPosition:position];
+RCT_EXPORT_METHOD(moveRight:(nonnull NSNumber *)reactTag)
+{
+    UITextField *view = (UITextField *)(((RCTBaseTextInputView*)[_bridge.uiManager viewForReactTag:reactTag]).backedTextInputView);
+    
+    UITextRange* range = view.selectedTextRange;
+    UITextPosition* position = range.end;
+    
+    if ([view comparePosition:range.start toPosition:range.end] == 0) {
+        position = [view positionFromPosition: position offset: 1];
+    }
+    
+    view.selectedTextRange = [view textRangeFromPosition: position toPosition:position];
 }
 
-RCT_EXPORT_METHOD(switchSystemKeyboard:(nonnull NSNumber*) reactTag) {
-  UITextView *view = [_bridge.uiManager viewForReactTag:reactTag];
-  UIView* inputView = view.inputView;
-  view.inputView = nil;
-  [view reloadInputViews];
-  view.inputView = inputView;
+RCT_EXPORT_METHOD(switchSystemKeyboard:(nonnull NSNumber*)reactTag)
+{
+    UITextField *view = (UITextField *)(((RCTBaseTextInputView*)[_bridge.uiManager viewForReactTag:reactTag]).backedTextInputView);
+    UIView* inputView = view.inputView;
+    view.inputView = nil;
+    [view reloadInputViews];
+    view.inputView = inputView;
+}
+
+RCT_EXPORT_METHOD(hideKeyboard:(nonnull NSNumber *)reactTag)
+{
+    UITextField *view = (UITextField *)(((RCTBaseTextInputView*)[_bridge.uiManager viewForReactTag:reactTag]).backedTextInputView);
+    view.inputView = nil;
+    [view reloadInputViews];
+    [view resignFirstResponder];
 }
 
 @end
--- ios/RNCustomKeyboardKit.podspec
+++ ios/RNCustomKeyboardKit.podspec
@@ -1,20 +1,24 @@
-require 'json'
-  
-package = JSON.parse(File.read(File.join(__dir__, '..', 'package.json')))
 
 Pod::Spec.new do |s|
   s.name         = "RNCustomKeyboardKit"
-  s.version      = package['version']
-  s.summary      = package['description']
-  s.license      = package['license']
-
-  s.authors      = package['author']
-  s.homepage     = package['homepage']
-
+  s.version      = "1.0.0"
+  s.summary      = "RNCustomKeyboardKit"
+  s.description  = <<-DESC
+                  RNCustomKeyboardKit
+                   DESC
+  s.homepage     = "www.ABC.com"
+  s.license      = "MIT"
+  # s.license      = { :type => "MIT", :file => "FILE_LICENSE" }
+  s.author             = { "ABC" => "ABC@domain.cn" }
   s.platform     = :ios, "7.0"
   s.source       = { :git => "https://github.com/author/RNCustomKeyboardKit.git", :tag => "master" }
-  s.source_files  = "RNCustomKeyboardKit/**/*.{h,m}"
+  s.source_files  = "*.{h,m}"
   s.requires_arc = true
 
+
   s.dependency "React"
+  #s.dependency "others"
+
 end
+
+  
